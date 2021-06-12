9d52cf639d72ac825e121b9c5c50ace2f1676f7d
张亚飞 <672233580@qq.com>
2021-06-12 16:16:50 +0800

    Initial Commit

---
+++ SwiftUI3CoreLocationUI/SwiftUI3CoreLocationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI3CoreLocationUIApp.swift
+//  SwiftUI3CoreLocationUI
+//
+//  Created by 张亚飞 on 2021/6/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI3CoreLocationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
